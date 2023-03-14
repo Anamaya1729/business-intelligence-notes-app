e34aea6d056e02d825b55017a286cc7d4431d864
Anamaya Sharma <sharma.011999@gmail.com>
2023-03-14 21:59:02 +0530

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import { Navbar, Nav } from 'react-bootstrap';
 function AppNavbar() {
   return (
     <Navbar bg="light" expand="md" className="mb-4">
-      <Navbar.Brand href="#" className="font-weight-bold text-uppercase">Anamaya is BORED!</Navbar.Brand>
+      <Navbar.Brand href="#" className="font-weight-bold text-uppercase">Anamaya was BORED!</Navbar.Brand>
       <Navbar.Toggle aria-controls="navbar-nav" />
       <Navbar.Collapse id="navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="#topics" className="text-uppercase">Topics</Nav.Link>
-          <Nav.Link href="#dss" className="text-uppercase">DSS</Nav.Link>
-          <Nav.Link href="#architecture" className="text-uppercase">Architectures</Nav.Link>
+          <Nav.Link href="#architecture" className="text-uppercase">Architecture</Nav.Link>
+          <Nav.Link href="#important" className="text-uppercase">Important Questions</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
